9247cfc345f847e23bad78ccdd332db806c92eae
Johnie Hjelm <johnie.hjelm@symbio.com>
2014-11-29 23:52:28 +0100

    Added news

--- web/controllers/home.php
+++ web/controllers/home.php
@@ -2,6 +2,18 @@
 
 <?php get_template_part('templates/pier'); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_format()); ?>
-<?php endwhile; ?>
+<div class="news container">
+  <h3 class="news-title"><?php _e( "Nyheter", "ungarh" ); ?></h3>
+
+  <div class="news__inner">
+    <?php
+      $args = array('posts_per_page' => 3);
+      $query = new WP_Query( $args );
+      while( $query->have_posts()) : $query->the_post();
+    ?>
+      <?php get_template_part('templates/news', get_post_format()); ?>
+    <?php endwhile; ?>
+  </div>
+  <!--/.news__inner -->
+</div>
+<!--/.news -->
